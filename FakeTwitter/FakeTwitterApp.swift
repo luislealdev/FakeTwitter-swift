fafb3fe234dbbd8e181764ace5aaed1ee1c6e269
Luis Leal <luisrrleal@gmail.com>
2023-03-08 12:09:31 -0600

    Initial Commit

---
+++ FakeTwitter/FakeTwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeTwitterApp.swift
+//  FakeTwitter
+//
+//  Created by Luis Leal on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FakeTwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
